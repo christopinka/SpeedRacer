0e659183f0fd9033f8f63df805f5a795029de08b
or wolfstein <gleeber@gmail.com>
2012-07-26 23:26:47 +0300

    first version of db and general spec for the site

---
+++ Database script/rentdb V-1.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 26, 2012 at 08:24 PM
+-- Server version: 5.5.20
+-- PHP Version: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rentdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car`
+--
+
+CREATE TABLE IF NOT EXISTS `car` (
+  `car_id` int(11) NOT NULL AUTO_INCREMENT,
+  `manufacturer` int(11) NOT NULL,
+  `brand` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `mileage` int(11) NOT NULL,
+  `rate_per_day` float NOT NULL,
+  `status` int(11) NOT NULL,
+  `current_dealership_id` int(11) NOT NULL,
+  PRIMARY KEY (`car_id`),
+  KEY `status` (`status`),
+  KEY `current_dealership_id` (`current_dealership_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car_additional_details`
+--
+
+CREATE TABLE IF NOT EXISTS `car_additional_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `car_id` int(11) NOT NULL,
+  `last_maintenance_date` date NOT NULL,
+  `last_maintenance_mileage` int(11) NOT NULL,
+  `total_money_earned` int(11) NOT NULL,
+  `total_money_spent` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `car_id` (`car_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car_spendings`
+--
+
+CREATE TABLE IF NOT EXISTS `car_spendings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `car_id` int(11) NOT NULL,
+  `amount` float NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car_status`
+--
+
+CREATE TABLE IF NOT EXISTS `car_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dealership`
+--
+
+CREATE TABLE IF NOT EXISTS `dealership` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `ciry` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ofers`
+--
+
+CREATE TABLE IF NOT EXISTS `ofers` (
+  `int` int(11) NOT NULL AUTO_INCREMENT,
+  `description` int(11) NOT NULL,
+  PRIMARY KEY (`int`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ofer_type`
+--
+
+CREATE TABLE IF NOT EXISTS `ofer_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `start_date` varchar(11) NOT NULL,
+  `end_date` varchar(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  `order_status` int(11) NOT NULL,
+  `start_km` int(11) NOT NULL,
+  `end_km` int(11) NOT NULL,
+  `start_dealership` int(11) NOT NULL,
+  `end_dealership` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `car_id` (`car_id`),
+  KEY `offer_id` (`offer_id`),
+  KEY `order_status` (`order_status`),
+  KEY `start_dealership` (`start_dealership`),
+  KEY `end_dealership` (`end_dealership`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_status`
+--
+
+CREATE TABLE IF NOT EXISTS `order_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `billing_address` varchar(100) NOT NULL,
+  `mailing_address` varchar(100) NOT NULL,
+  `user_type` int(11) NOT NULL,
+  `ssid` varchar(50) NOT NULL COMMENT 'social security id',
+  `licence_number` varchar(50) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `user_type` (`user_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_type`
+--
+
+CREATE TABLE IF NOT EXISTS `user_type` (
+  `id` int(11) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `car`
+--
+ALTER TABLE `car`
+  ADD CONSTRAINT `car_ibfk_2` FOREIGN KEY (`current_dealership_id`) REFERENCES `dealership` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`status`) REFERENCES `car_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `car_additional_details`
+--
+ALTER TABLE `car_additional_details`
+  ADD CONSTRAINT `car_additional_details_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `car` (`car_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_6` FOREIGN KEY (`end_dealership`) REFERENCES `dealership` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `car` (`car_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order_ibfk_3` FOREIGN KEY (`offer_id`) REFERENCES `ofers` (`int`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `order_ibfk_4` FOREIGN KEY (`order_status`) REFERENCES `order_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order_ibfk_5` FOREIGN KEY (`start_dealership`) REFERENCES `dealership` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
